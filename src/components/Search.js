049d5004310ea4ea817aca2e9f380b4cc4200c46
John <johnmpalmgren@gmail.com>
2021-11-01 20:24:35 +0000

    added guest logic to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,63 @@
-import { Fragment, useState } from "react";
-import classes from "./Search.module.css"
+import { Fragment, useState, useReducer } from "react";
+import classes from "./Search.module.css";
 import SearchIcon from "@material-ui/icons/Search";
 
-
 const Search = (props) => {
-  const [clicked, setClicked] = useState(false)
-  const [userInput, setUserInput] = useState("")
+  const [clicked, setClicked] = useState(false);
 
+  const guestReducer = (state, action) => {
+
+    if (action.type === "ADD_ADULT") {
+      return (state = { guests: state.guests + 1, adults: state.adults + 1, children: state.children });
+    }
+    if (action.type === "REMOVE_ADULT") {
+      if (state.adults < 1) {
+        return {
+          guests: state.guests,
+          adults: state.adults,
+          children: state.children
+        };
+      }
+      return (state = { guests: state.guests - 1, adults: state.adults - 1, children: state.children });
+    }
+    if (action.type === "ADD_CHILD") {
+      return (state = {
+        guests: state.guests + 1,
+        adults: state.adults,
+        children: state.children + 1,
+      });
+    }
+    if (action.type === "REMOVE_CHILD") {
+      if (state.children < 1) {
+        return {
+          guests: state.guests,
+          adults: state.adults,
+          children: state.children,
+        };
+      }
+      return (state = {
+        guests: state.guests - 1,
+        adults: state.adults,
+        children: state.children - 1,
+      });
+    }
+  };
+
+  const [guestState, dispatchGuest] = useReducer(guestReducer, {
+    guests: 0,
+    adults: 0,
+    children: 0,
+  });
 
   const expandForm = () => {
     setClicked(true);
-  }
+  };
 
   const contractForm = () => {
-        setClicked(false);
-  }
-  // const inputHandler = (event) => {
-  //   setUserInput(event.target.value);
-  // }
+    setClicked(false);
+  };
 
-  let locations = []
+  let locations = [];
   let locationCheck = [];
 
   for (let i of props.propertyData) {
@@ -29,67 +67,76 @@ const Search = (props) => {
     }
   }
 
-    return (
-      <Fragment>
-        <div
-          onClick={expandForm}
-          className={clicked ? classes.expandedForm : classes.form}
-        >
-          <div className={classes.input}>{locations[0]}</div>
-          <div className={classes.input}>Add guests</div>
-          <div className={classes.buttonWrapper}>
-            <button
-              className={clicked ? classes.expandedButton : classes.button}
-            >
-              <SearchIcon className={classes.icon} />
-              {clicked ? <span className={classes.text}>Search</span> : null}
-            </button>
-          </div>
+  const addAdult = () => {
+    dispatchGuest({ type: "ADD_ADULT" });
+  };
+
+  const removeAdult = () => {
+    dispatchGuest({ type: "REMOVE_ADULT" });
+  };
+
+  const addChild = () => {
+    dispatchGuest({ type: "ADD_CHILD" });
+  };
+
+  const removeChild = () => {
+    dispatchGuest({ type: "REMOVE_CHILD" });
+  };
+
+  return (
+    <Fragment>
+      <div
+        onClick={expandForm}
+        className={clicked ? classes.expandedForm : classes.form}
+      >
+        <div className={classes.input}>{locations[0]}</div>
+        <div className={classes.input}>
+          {guestState.guests < 1 ? "Add guests" : guestState.guests}
         </div>
-        <div className={clicked ? classes.expandedWrapper : classes.hidden}>
-          <div className={classes.expandedItems}>
-            <ul>
-              {locations.map((item) => {
-                return <li>{item}</li>;
-              })}
-            </ul>
-          </div>
-          <div className={classes.expandedItems}>guests</div>
-          <div className={classes.expandedItems}>placeholder</div>
+        <div className={classes.buttonWrapper}>
+          <button className={clicked ? classes.expandedButton : classes.button}>
+            <SearchIcon className={classes.icon} />
+            {clicked ? <span className={classes.text}>Search</span> : null}
+          </button>
         </div>
+      </div>
+      <div className={clicked ? classes.expandedWrapper : classes.hidden}>
+        <div className={classes.expandedItems}>
+          <ul>
+            {locations.map((item) => {
+              return <li>{item}</li>;
+            })}
+          </ul>
+        </div>
+        <div className={classes.expandedItems}>
+          <span>Adults </span>
+          <span>
+            <br /> Ages 13 and above <br />
+          </span>
+          <button onClick={removeAdult}>-</button>
+          <div className={classes.number}>{guestState.adults}</div>
+          <button onClick={addAdult}>+</button>
 
-        {clicked ? (
-          <div onClick={contractForm} className={classes.backdrop}></div>
-        ) : (
-          <div></div>
-        )}
+          <span>
+            <br /> Children
+          </span>
+          <span>
+            <br /> 2-12 <br />
+          </span>
+          <button onClick={removeChild}>-</button>
+          <div className={classes.number}>{guestState.children}</div>
+          <button onClick={addChild}>+</button>
+        </div>
+        <div className={classes.expandedItems}></div>
+      </div>
 
-        {/* <form
-          onClick={expandForm}
-          className={clicked ? classes.expandedForm : classes.form}
-        >
-          <input
-            onChange={inputHandler}
-            className={classes.input}
-            type="text"
-            placeholder="Location"
-          />
-          <input
-            className={classes.input}
-            type="number"
-            placeholder="Add guests"
-          />
-          <div className={classes.buttonWrapper}>
-            <button
-              className={clicked ? classes.expandedButton : classes.button}
-            >
-              <SearchIcon className={classes.icon} />
-              {clicked ? <span className={classes.text}>Search</span> : null}
-            </button>
-          </div>
-        </form> */}
-      </Fragment>
-    );
-}
+      {clicked ? (
+        <div onClick={contractForm} className={classes.backdrop}></div>
+      ) : (
+        <div></div>
+      )}
+    </Fragment>
+  );
+};
 
-export default Search
+export default Search;
